ad4c93dcaa2645ca800ac6475522c6ffe84def84
amanmukri03 <amanmukri2003@gmail.com>
2026-01-10 23:55:52 +0530

    Fixed the routes issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-import React, { useEffect } from 'react'
-import'./styles/global.css'
-import {BrowserRouter, Routes, Route, useNavigate, HashRouter } from 'react-router-dom'
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Board from './pages/Board';
-import Dashboard from './pages/Dashboard';
-import ProtectedRoute from './components/ProtectedRoute';
+import './styles/global.css'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Board from './pages/Board'
+import Dashboard from './pages/Dashboard'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
-
   return (
-    <div className='app-container'>
-      <Routes>
-        <Route path="/board" element={<Board/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
-        <Route path="/board" element={<ProtectedRoute><Board/></ProtectedRoute>}/>
-        <Route path="*" element={<Login/>}/>
+    <div className="app-container">
+      <HashRouter>
 
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Board />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/board"
+            element={
+              <ProtectedRoute>
+                <Board />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
-import { HashRouter } from 'react-router-dom';
-
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    
+      <App />
 
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>
-  ,
+  </Provider>
 )
